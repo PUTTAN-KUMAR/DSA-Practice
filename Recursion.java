a5d8b3e0827a7fa13edadc58344388f2db411066
puttan kumar <puttankumar76@gmail.com>
2025-03-30 21:37:27 +0530

    add new file

--- Recursion.java
+++ Recursion.java
@@ -8,6 +8,7 @@ public class Recursion {
         printDec(n-1);
 
     }
+    // print number increasing order
     public static void printInc(int n){
         if(n == 1){
             System.out.print(n+" ");
@@ -16,10 +17,56 @@ public class Recursion {
         printInc(n-1);
         System.out.print(n+" ");
     }
+    // fib..................................******** no
+    public static int fib(int n){
+        if(n == 0 || n ==1){
+            return n;
+        }
+        int fnm1 = fib(n-1);
+        int fnm2 = fib(n-2);
+        int fn = fnm1 + fnm2;
+        return fn;
+    
+    
+
+    }
+    public static boolean isSortd(int arr[],int i){
+        if(i == arr.length-1){
+            return true;
+        }
+        if(arr[i] > arr[i+1]){
+            return false;
+        }
+        return isSortd(arr, i+1);
+
+    }
+    public static int firstOccurence(int arr[] , int key, int i ){
+        if(arr[i] == key){
+            return i;
+        }
+        return firstOccurence(arr, key, i+1);
+
+    } 
+    public static int tilingProblem(int n){
+           if(n == 0 || n == 1){
+            return 1;
+           }
+            int fnm1 = tilingProblem(n-2);
+            int fnm2 = tilingProblem(n-2);
+            int totWays = fnm1+ fnm2;
+            return totWays;
+
+           }
+    
+
+
+
 
     public static void main(String args[]){
-        int n=10;
-        printInc(n);
+        System.out.println(tilingProblem(3));
+        int arr[]={8,3,6,5,4,3,5,6,7,};
+     System.out.println(firstOccurence(arr, 5, 0));
 
     }
 }
+// end--------------------------------------------------
